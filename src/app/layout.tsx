7b430c2b03fd5e868882e67702dbf19dbde22e59
Santiago Tourn <111823540+HSPower255@users.noreply.github.com>
2023-04-30 00:32:56 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <title>Chat App by Santiago Tourn</title>
-        <meta name="description" content=" " />
+        <title>Chat App</title>
+        <meta name="description" content="Experience real-time messaging with our web application that features Google sign-in, friend requests, and secure authentication. With a modern design made with Tailwind and fully responsive to all screen resolutions, users can easily connect with friends and receive real-time notifications even when they are away from the chat.  " />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:image" content="ogimg.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <link rel="icon" href="logo.png" />
       </head>
       <body>
         <Providers>{children}</Providers>
